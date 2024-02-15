0beb768b7a396ce8c5b10d9a44a4978a2c89fc54
Adam Carpenter <adamcarpenter86@gmail.com>
2024-02-15 10:25:07 -0700

    Remove logs

--- src/serialize.ts
+++ src/serialize.ts
@@ -117,8 +117,6 @@ const encodeV1 = (key: string, value: unknown): Buffer | null => {
 
     case 'privateTxnCount':
     case 'txnCount': {
-      console.log('txnCount: ', value)
-      console.log('privateTxnCount: ', value)
       const encodedLengthAndValue = int16Encoder(value as number)
       return Buffer.concat([tagBuf, encodedLengthAndValue])
     }
